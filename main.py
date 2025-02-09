ce975bd6d32e021f4921302df75c75448f4acf56
Absalyamov_Salavat <business.stushor.jap@gmail.com>
2025-02-09 19:15:33 +0500

    Исправлены потенциальные баги и переписан код

--- main.py
+++ main.py
@@ -4,17 +4,27 @@ from dotenv import load_dotenv
 import os
 
 
-load_dotenv()
+def is_shortened_link(token, url):
+    parsed_url = urlparse(url)
+    key = parsed_url.path.lstrip('/')
 
+    payload = {
+        "v": "5.199",
+        "access_token": token,
+        "key": key,
+        "interval": "forever",
+        "extended": 0
+    }
 
-def is_shortened_link(url):
-    try:
-        result = urlparse(url)
-        if not all([result.netloc]):
+    response = requests.get('https://api.vk.ru/method/utils.getLinkStats', params=payload)
+    response.raise_for_status()
+    response_data = response.json()
+
+    if 'error' in response_data:
+        if response_data['error']['error_code']:
             return False
-        return url.startswith("https://vk.cc/")
-    except ValueError:
-        return False
+        raise Exception(f"API Error: {response_data['error']['error_msg']}")
+    return True
 
 
 def shorten_link(token, url):
@@ -24,27 +34,17 @@ def shorten_link(token, url):
         "url": url,
         "private": 0
     }
-    try:
-        response = requests.get('https://api.vk.ru/method/utils.getShortLink', params=payload)
-        response.raise_for_status()
-        data = response.json()
-        if 'error' in data:
-            raise Exception(f"API Error: {data['error']['error_msg']}")
-        short_link = data['response']['short_url']
-        return short_link
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-    except Exception as err:
-        print(f"An error occurred: {err}")
+    response = requests.get('https://api.vk.ru/method/utils.getShortLink', params=payload)
+    response.raise_for_status()
+    response_data = response.json()
+    if 'error' in response_data:
+        raise Exception(f"API Error: {response_data['error']['error_msg']}")
+    short_link = response_data['response']['short_url']
+    return short_link
 
 
 def count_clicks(token, short_link):
-    if not short_link.startswith("https://vk.cc/"):
-        print("Ошибка: Неверный формат сокращённой ссылки.")
-        return None
-
     key = short_link.split('/')[-1]
-
     payload = {
         "v": "5.199",
         "access_token": token,
@@ -52,49 +52,41 @@ def count_clicks(token, short_link):
         "interval": "forever",
         "extended": 0
     }
+
+    response = requests.get('https://api.vk.ru/method/utils.getLinkStats', params=payload)
+    response.raise_for_status()
+    response_data = response.json()
+    if 'error' in response_data:
+        raise Exception(f"API Error: {response_data['error']['error_msg']}")
+    clicks_count = response_data['response']['stats'][0]['views']
+    return clicks_count
+
+
+def main():
     try:
-        response = requests.get('https://api.vk.ru/method/utils.getLinkStats', params=payload)
-        response.raise_for_status()
-        data = response.json()
-        if 'error' in data:
-            raise Exception(f"API Error: {data['error']['error_msg']}")
-        clicks_count = sum(item['views'] for item in data['response']['stats'])
-        return clicks_count
+        load_dotenv()
+        token = os.getenv("VK_ACCESS_TOKEN")
+
+        if not token:
+            print("Ошибка: Токен VK_ACCESS_TOKEN не найден в переменных окружения.")
+            return
+
+        user_input = input("Введите URL для сокращения: ").strip()
+        if not user_input:
+            print("Ошибка: Введённая строка пуста.")
+            return
+
+        if is_shortened_link(token, user_input):
+            clicks_count = count_clicks(token, user_input)
+            print(f"Количество кликов: {clicks_count}")
+        else:
+            short_link = shorten_link(token, user_input)
+            if short_link:
+                print(f"Сокращённая ссылка: {short_link}")
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
     except Exception as err:
         print(f"An error occurred: {err}")
-
-
-def main():
-    token = os.getenv("VK_ACCESS_TOKEN")
-    if not token:
-        print("Ошибка: Токен VK_ACCESS_TOKEN не найден в переменных окружения.")
-        return
-
-    user_input = input("Введите URL для сокращения: ").strip()
-    if not user_input:
-        print("Ошибка: Введённая строка пуста.")
-        return
-
-    if is_shortened_link(user_input):
-        print("Ссылка уже сокращена.")
-        clicks_count = count_clicks(token, user_input)
-        if clicks_count is not None:
-            print(f"Количество кликов: {clicks_count}")
-        else:
-            print("Не удалось получить статистику кликов.")
-    else:
-        short_link = shorten_link(token, user_input)
-        if short_link:
-            print(f"Сокращённая ссылка: {short_link}")
-            clicks_count = count_clicks(token, short_link)
-            if clicks_count is not None:
-                print(f"Количество кликов: {clicks_count}")
-            else:
-                print("Не удалось получить статистику кликов.")
-        else:
-            print("Не удалось получить сокращённую ссылку.")
 
 
 if __name__ == "__main__":
